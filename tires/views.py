974ab42bb60a6dca44b05d87671e9afc52dfb59f
lunayyko <youryoung.ko@gmail.com>
2021-11-27 17:42:25 +0900

    Add: simpleJWT추가

--- tires/views.py
+++ tires/views.py
@@ -1,6 +1,7 @@
 from rest_framework             import generics, viewsets
 from rest_framework.generics    import RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators  import action
 from rest_framework.response    import Response
 
 from .models                    import Tire
@@ -9,24 +10,10 @@ from .serializers               import TireSerializers
 class TireViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class   = TireSerializers
-
-    def create(self, request):
-        # """
-        # 유저별 타이어 정보 저장
-        # POST /tires
-
-        # data params
-        # {id : 유저아이디, trimid : 타이어번호}
-        # """
-        print(request.data)
+    
+    def register_tires(self, request):
         tire = Tire.objects.create(
             id = request.data[id],
             trimid = request.data[trimid]
         )
-        print(id)
-
         return Response(self.get_serializer(tire).data, status=status.HTTP_201_CREATED)
-
-# class SearchTireView(RetrieveAPIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class   = TireSerializer
